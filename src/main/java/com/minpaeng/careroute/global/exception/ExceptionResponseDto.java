6c85a3e5790e8c29c5a21085eb3b3534c2779852
권민정 <22alswjd@naver.com>
2024-06-04 10:58:22 +0900

    feat: 예외처리 기본 구현

--- src/main/java/com/minpaeng/careroute/global/exception/ExceptionResponseDto.java
+++ src/main/java/com/minpaeng/careroute/global/exception/ExceptionResponseDto.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Builder
 public class ExceptionResponseDto {
-    private HttpStatus status;
-    private int code;
+    private int statusCode;
     private String message;
 }
