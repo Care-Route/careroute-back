0843b14e3c2e063aa97ff4d7ac4c6e42af2e22b0
권민정 <22alswjd@naver.com>
2024-06-09 02:45:13 +0900

    feat: 스웨거 설정

--- src/main/java/com/minpaeng/careroute/domain/route/controller/RouteController.java
+++ src/main/java/com/minpaeng/careroute/domain/route/controller/RouteController.java
@@ -5,6 +5,7 @@ import com.minpaeng.careroute.domain.route.response.BookmarkListResponse;
 import com.minpaeng.careroute.domain.route.response.BookmarkSaveResponse;
 import com.minpaeng.careroute.domain.route.service.BookmarkService;
 import com.minpaeng.careroute.global.dto.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,18 +27,21 @@ import java.security.Principal;
 public class RouteController {
     private final BookmarkService bookmarkService;
 
+    @Operation(summary = "즐겨찾기 등록", description = "장소 즐겨찾기 등록 API")
     @PostMapping("/bookmark")
     public BookmarkSaveResponse saveSpotBookmark(Principal principal,
                                                  @RequestBody BookmarkSaveRequest request) {
         return bookmarkService.saveSpotBookmark(principal.getName(), request);
     }
 
+    @Operation(summary = "즐겨찾기 해제", description = "장소 즐겨찾기 해제 API")
     @DeleteMapping("/bookmark/{bookmarkId}")
     public BaseResponse deleteSpotBookmark(Principal principal,
                                            @PathVariable int bookmarkId) {
         return bookmarkService.deleteBookmark(principal.getName(), bookmarkId);
     }
 
+    @Operation(summary = "즐겨찾기 목록 조회", description = "즐겨찾기 목록 조회 API")
     @GetMapping("/bookmark")
     public BookmarkListResponse getSpotBookmarks(Principal principal) {
         return bookmarkService.getSpotBookmarks(principal.getName());
